f9ad4a5f69603c73904a1dd173dce7502ff52fba
rupansh <rupanshsekar@hotmail.com>
2023-03-28 03:14:37 +0530

    models/basic_net: move __block to a separate class

--- models/basic_net.py
+++ models/basic_net.py
@@ -1,5 +1,21 @@
 import torch.nn as nn
 
+class NNetBlock(nn.Module):
+    def __init__(self, inchannels, outchannels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.MaxPool1d(2, 2),
+            nn.Dropout(p=0.1, inplace=True),
+            nn.Conv1d(inchannels, outchannels, 5, padding=2),
+            nn.LeakyReLU(0.1),
+            nn.Conv1d(outchannels, outchannels, 5, padding=2),
+            nn.LeakyReLU(0.1),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
 class NNet(nn.Module):
     def __init__(self, in_channels=32, out_channels=6):
         super(NNet, self).__init__()
@@ -11,23 +27,13 @@ class NNet(nn.Module):
             nn.Conv1d(self.hidden, self.hidden, 7, padding=3),
         )
         for i in range(6):
-            self.net.add_module('conv{}'.format(i), \
-                                self.__block(self.hidden, self.hidden))
+            self.net.add_module('conv{}'.format(i), NNetBlock(self.hidden, self.hidden))
+
         self.net.add_module('final', nn.Sequential(
             nn.Conv1d(self.hidden, out_channels, 1),
             nn.Sigmoid()
         ))
         
-    def __block(self, inchannels, outchannels):
-        return nn.Sequential(
-            nn.MaxPool1d(2, 2),
-            nn.Dropout(p=0.1, inplace=True),
-            nn.Conv1d(inchannels, outchannels, 5, padding=2),
-            nn.LeakyReLU(0.1),
-            nn.Conv1d(outchannels, outchannels, 5, padding=2),
-            nn.LeakyReLU(0.1),
-        )
-    
     def forward(self, x):
         pred = self.net(x)
         return pred.squeeze(dim=-1)
